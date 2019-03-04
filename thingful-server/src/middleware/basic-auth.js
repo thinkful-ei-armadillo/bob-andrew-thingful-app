813736dd5ef2d2740bd256ae77ea421dafc2a58f
bobnearents <bobnearents@yahoo.com>
2019-03-04 09:29:03 -0800

    added base authentication

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,11 +1,26 @@
 'use strict';
 
 function requireAuth(req, res, next) {
-  console.log('requireAuth');
-  console.log(req.get('Authorization'));
+  const authToken = req.get('Authorization') || '';
+
+  let bearerToken;
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({error: 'Missing bearer token'});
+  } else {
+    bearerToken = authToken.slice(7, authToken.length)
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer 
+    .from(bearerToken)
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({error: 'Unautorized request'});
+  }
   next();
 }
 
 module.exports = {
   requireAuth,
-}
+};
