ccac8459eefad86b7b96425a910839c0cc1d7744
bobnearents <bobnearents@yahoo.com>
2019-03-04 09:15:13 -0800

    started protected endpoints

---
+++ thingful-server/src/middleware/basic-auth.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function requireAuth(req, res, next) {
+  console.log('requireAuth');
+  console.log(req.get('Authorization'));
+  next();
+}
+
+module.exports = {
+  requireAuth,
+}
